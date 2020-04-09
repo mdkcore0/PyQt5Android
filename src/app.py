928801334ca2c27ff36cd2129417cda0ff041097
Rodrigo Oliveira <rodrigo.oliveira@byne.com.br>
2020-04-09 19:22:49 -0300

    YAAAY

--- src/app.py
+++ src/app.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 
 import sys
-#import os
+import os
 
 from PyQt5.QtCore import Qt
-# from PyQt5.QtCore import QUrl
+from PyQt5.QtCore import QUrl
 from PyQt5.QtQuick import QQuickView
 from PyQt5.QtWidgets import QApplication
 
@@ -13,11 +13,16 @@ if __name__ == '__main__':
 
     view = QQuickView()
 
+    # Android had to add src/ beforme main.qml
     # view.setSource(QUrl.fromLocalFile(
+        # os.path.join(os.path.dirname(__file__), 'src/main.qml')))
         # os.path.join(os.path.dirname(__file__), 'main.qml')))
+    # this works too on Android:
+    view.setSource(QUrl("qrc:/src/main.qml"))
 
-    view.resize(300, 300)
-    view.setColor(Qt.green)
+    # view.resize(300, 300)
+    view.setColor(Qt.black)
     view.showFullScreen()
+    # view.show()
 
     sys.exit(app.exec_())
