199c383e55003cb6b1954787958fc700ec7f839b
Rodrigo Oliveira <rodrigo.oliveira@byne.com.br>
2020-04-13 20:36:13 -0300

    HERE WE GO

---
+++ env.sh
@@ -0,0 +1,8 @@
+#export ANDROID_NDK_ROOT=/home/rodrigo/downloads/Android/ndk/android-ndk-r21-linux-x86_64/android-ndk-r21
+export ANDROID_NDK_ROOT=/home/rodrigo/downloads/Android/ndk/android-ndk-r19c-linux-x86_64/android-ndk-r19c
+#export ANDROID_NDK_PLATFORM=android-17 (android 4.2)
+export ANDROID_NDK_PLATFORM=android-26
+export ANDROID_HOME=/home/rodrigo/downloads/Android/sdk
+export ANDROID_SDK_ROOT=$ANDROID_HOME
+export JAVA_HOME=/usr/lib64/java
+export PATH=$ANDROID_HOME/tools/bin:$ANDROID_NDK_ROOT:$PATH
